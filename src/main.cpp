2b2adbb60fde61b39a2fd2f5832e1daed05a6208
Jeb Nicholson <26765018+jeb5@users.noreply.github.com>
2023-08-24 16:22:40 +1200

    Added vite-plugin-singlefile

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ int main()
   w.set_title("Login Languish");
   w.set_size(WINDOW_WIDTH, WINDOW_HEIGHT, WEBVIEW_HINT_FIXED);
   w.navigate("file://" + resourcesPath + "/index.html");
-  w.bind("testFunction", onDocumentLoadCallback, &w); 
+  w.bind("documentLoadCallback", onDocumentLoadCallback, &w); 
   w.run();
   return 0;
 }
@@ -32,16 +32,8 @@ int main()
 void onDocumentLoadCallback(const std::string /*&seq*/, const std::string &req, void * arg) {
   webview::webview &w = *static_cast<webview::webview *>(arg);
 
-  std::cout << "Received message from JS: " << req << std::endl;
-  w.eval("document.getElementById('title').innerHTML = 'Hello from C++';");
+  std::cout << "Received json message from JS: " << req << std::endl;
   if (!__DEBUG__) {
     w.eval("window.addEventListener('contextmenu', (event) => event.preventDefault());"); // Prevents use of the context menu
   }
-  std::string js = loadStringFromFile(resourcesPath + "/assets/index.js");
-  std::cout << "JS: " << js << std::endl;
-  w.eval(js);
-  std::string css = loadStringFromFile(resourcesPath + "/assets/index.css");
-  std::cout << "CSS: " << css << std::endl;
-  w.eval("var style = document.createElement('style'); style.innerHTML = `" + css + "`; document.head.appendChild(style);");
-
 }
