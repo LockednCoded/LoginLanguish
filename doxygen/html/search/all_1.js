63cec62ef51304d6b184d72e80f8e374e1ca4f8c
Jeb Nicholson <26765018+jeb5@users.noreply.github.com>
2023-08-25 15:06:38 +1200

    Removed doxygen build

--- doxygen/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['loadstringfromfile_0',['loadStringFromFile',['../utils_8cpp.html#a9bd25f54f8e4bc21442fac7be1c59940',1,'loadStringFromFile(std::string filepath):&#160;utils.cpp'],['../utils_8h.html#a9bd25f54f8e4bc21442fac7be1c59940',1,'loadStringFromFile(std::string filepath):&#160;utils.cpp']]]
-];
