ffb2d30f2f2231d86378f3f5e047bc03602364db
voidinterface <cameron@voidinterface.net>
2023-09-07 02:31:47 +1200

    Major refactor project structure and cmake

--- src/test/test.h
+++
@@ -1,4 +0,0 @@
-#include <string>
-#include <stdio.h>
-#include <gtest/gtest.h>
-#include "../compatibility_utils.h"
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,10 +1,13 @@
-#include "test.h"
+#include <string>
+#include <stdio.h>
+#include <gtest/gtest.h>
+#include "compatibility_utils.h"
 
 // Demonstrate some basic assertions.
 TEST(Tests, FindResourcesPath) {
   std::string resourcesPath = getResourcesPath();
   std::cout << "Resources path: " << resourcesPath << std::endl;
   //Expect resourcesPath to end with "build". This is because the test is run from the build directory. On macOS, in the bundled executable, the resourcesPath will end with "AppName.app/Contents/Resources".
-  std::string expected = "build";
+  std::string expected = "Debug";
   EXPECT_EQ(resourcesPath.substr(resourcesPath.size() - expected.size()), expected);
 }
