f642ec9962bf23c264f579cc92fd12aab9aee94b
Jeb Nicholson <26765018+jeb5@users.noreply.github.com>
2023-09-08 14:46:59 +1200

    Fixed up runtime output directory & mac build

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,6 +8,6 @@ TEST(Tests, FindResourcesPath) {
   std::string resourcesPath = getResourcesPath();
   std::cout << "Resources path: " << resourcesPath << std::endl;
   //Expect resourcesPath to end with "build". This is because the test is run from the build directory. On macOS, in the bundled executable, the resourcesPath will end with "AppName.app/Contents/Resources".
-  std::string expected = "Debug";
+  std::string expected = "bin";
   EXPECT_EQ(resourcesPath.substr(resourcesPath.size() - expected.size()), expected);
 }
