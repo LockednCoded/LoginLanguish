14abd808cd6a8231d1ffdc42ab0ec443ec711c94
seaja052 <seaja052@student.otago.ac.nz>
2023-09-15 22:09:48 +1200

    removed unused PW_validator files

--- src/game/password_validator.cpp
+++
@@ -1,8 +0,0 @@
-#include "password_validator.h"
-
-bool PasswordValidator::Validate(std::string password) {
-    //regex to check if password contains only alphanumeric characters
-    std::regex regex = std::regex("[a-zA-Z0-9]+", std::regex::ECMAScript);
-    
-    return (password.length() >= 1 && !std::regex_match(password, regex));
-}
