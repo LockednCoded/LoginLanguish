0bea539ee0f37aa14d7ced1a6c72b09f8e30061f
seaja052 <seaja052@student.otago.ac.nz>
2023-09-15 12:49:37 +1200

    included vector in utils.h

--- src/game/password_validator.cpp
+++ src/game/password_validator.cpp
@@ -3,6 +3,6 @@
 bool PasswordValidator::Validate(std::string password) {
     //regex to check if password contains only alphanumeric characters
     std::regex regex = std::regex("[a-zA-Z0-9]+", std::regex::ECMAScript);
-
+    
     return (password.length() >= 1 && !std::regex_match(password, regex));
 }
