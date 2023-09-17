5c42304a955ed9d624c80e9ccab1934c8e38dfbc
Jeb Nicholson <26765018+jeb5@users.noreply.github.com>
2023-09-17 18:57:33 +1200

    Added password reveal ability

--- static_site/src/components/TextField.tsx
+++ static_site/src/components/TextField.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import useSmoothUpdate from "../scripts/useSmoothUpdate";
+import { Icon } from "@iconify/react";
 
 export default function TextField(props: {
   type: "text" | "password" | "date";
@@ -14,6 +15,8 @@ export default function TextField(props: {
     props.onChange
   );
 
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className={props.className}>
       <div>
@@ -23,23 +26,43 @@ export default function TextField(props: {
         >
           {props.name}
         </label>
-        <input
-          className={`border rounded border-black w-full h-9 p-2 box-border outline-none disabled:opacity-75 select-text ${
-            props.type == "password"
-              ? "[-webkit-text-security:disc] tracking-wider"
-              : ""
-          }`}
-          data-test-id={`text-field-${props.name}`}
-          type={{ password: "text", text: "text", date: "date" }[props.type]}
-          name={props.name}
-          id={props.name}
-          onBlur={() => setFieldIsActive(false)}
-          onFocus={() => setFieldIsActive(true)}
-          autoComplete="off"
-          value={stableValue}
-          onChange={updateValue}
-          disabled={props.disabled || false}
-        />
+        <div className="w-full relative">
+          <input
+            className={`border rounded border-black w-full h-9 p-2 box-border outline-none disabled:opacity-75 select-text ${
+              props.type == "password" && !showPassword
+                ? "[-webkit-text-security:disc] tracking-wider"
+                : ""
+            }`}
+            data-test-id={`text-field-${props.name}`}
+            type={{ password: "text", text: "text", date: "date" }[props.type]}
+            name={props.name}
+            id={props.name}
+            onBlur={() => setFieldIsActive(false)}
+            onFocus={() => setFieldIsActive(true)}
+            autoComplete="off"
+            autoCorrect="off"
+            autoCapitalize="off"
+            spellCheck="false"
+            value={stableValue}
+            onChange={(e) => {
+              if (showPassword) setShowPassword(false);
+              updateValue(e);
+            }}
+            disabled={props.disabled || false}
+          />
+          {props.type == "password" && (
+            <div
+              className="absolute top-1/2 transform -translate-y-1/2 right-2 text-lg cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <Icon icon="tabler:eye" />
+              ) : (
+                <Icon icon="tabler:eye-closed" />
+              )}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
